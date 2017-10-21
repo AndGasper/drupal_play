34802730b20672cd4f79ce7a84a1091da9aaf888
AndGasper <24193043+AndGasper@users.noreply.github.com>
2017-10-20 21:10:28 -0700

    Added ctools, views, and webform contrib modules.

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # Drupal 7 snippets
-Mostly just messing around with Drupal, so I don't lose my job.
+~~Mostly just messing around with Drupal, so I don't lose my job.~~
+Er, I mean: 
+> Mostly just messing around with Drupal ***for the love of the game*** 
 
-I've only committed the sites/all/modules/custom folder for now.
+[Interesting Stack Overflow Link about scope](https://stackoverflow.com/questions/16959576/reference-what-is-variable-scope-which-variables-are-accessible-from-where-and)
+*Related to the `&$args` in `hook_type($args [, &$args])`*
+
+
---
+++ sites/all/modules/custom/framework_add/framework_add_parent_form.inc.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * Implements hook_form($form, &$form_state)
+ * 
+ * Note - hook_form() is supposed to return a "renderable" array of items
+ * Meaning that this is only part of the equation
+ * 
+ */
+function framework_add_parent_form($form, &$form_state) {
+    /**
+     * If you're wondering what the deal with the &$form_state is, you're not alone
+     * &$variable is PHP's way of passing a function of an argument "by reference"
+     * "by reference" vs. "by value" => Seems like a scope issue
+     * By reference allows your edits inside the scope of a function to take effect outside of that function.
+     * Hmmm...I wonder what the limit is on the scope
+     * 
+     */
+    $form = array();
+
+    $form['title'] = array(
+        '#type' => 'textfield',
+        '#title' => t('Form Title')
+    );
+
+    return $form;
+}
+
+function framework_add_parent_form_validate($form, &$form_state) {
+    dpm($form_state, '$form_state'); 
+}
+
+function framework_add_parent_form_submit($form, &$form_state) {
+
+}
+
+?>
